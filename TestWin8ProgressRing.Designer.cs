36d1232e0e9f7838e4f7e657444cd0e223829747
saeed rajayi <Saeedrajayi@gmail.com>
2024-05-20 11:14:05 +0330

    Add project files.

---
+++ TestWin8ProgressRing.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace TestWin8ProgressRing
+    {
+    partial class TestWin8ProgressRing
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+            {
+            if ( disposing && ( components != null ) )
+                {
+                components.Dispose ( );
+                }
+            base.Dispose ( disposing );
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ( )
+            {
+            this.start_stop_BUT = new System.Windows.Forms.Button ( );
+            this.pulse_BUT = new System.Windows.Forms.Button ( );
+            this.panel_background_BUT = new System.Windows.Forms.Button ( );
+            this.control_height_LAB = new System.Windows.Forms.Label ( );
+            this.control_height_NUD = new System.Windows.Forms.NumericUpDown ( );
+            this.panel_background_LAB = new System.Windows.Forms.Label ( );
+            this.colors_GB = new System.Windows.Forms.GroupBox ( );
+            this.indicator_color_BUT = new System.Windows.Forms.Button ( );
+            this.indicator_LAB = new System.Windows.Forms.Label ( );
+            this.indicator_type_GB = new System.Windows.Forms.GroupBox ( );
+            this.pulsed_RB = new System.Windows.Forms.RadioButton ( );
+            this.animated_RB = new System.Windows.Forms.RadioButton ( );
+            this.refresh_interval_LAB = new System.Windows.Forms.Label ( );
+            this.refresh_interval_NUD = new System.Windows.Forms.NumericUpDown ( );
+            this.control_PAN = new System.Windows.Forms.Panel ( );
+            this.win8_progress_ring = new Win8ProgressRing.Win8_ProgressRing ( );
+            this.exit_BUT = new System.Windows.Forms.Button ( );
+            ( ( System.ComponentModel.ISupportInitialize ) ( this.control_height_NUD ) ).BeginInit ( );
+            this.colors_GB.SuspendLayout ( );
+            this.indicator_type_GB.SuspendLayout ( );
+            ( ( System.ComponentModel.ISupportInitialize ) ( this.refresh_interval_NUD ) ).BeginInit ( );
+            this.control_PAN.SuspendLayout ( );
+            this.SuspendLayout ( );
+            // 
+            // start_stop_BUT
+            // 
+            this.start_stop_BUT.BackColor = System.Drawing.SystemColors.Control;
+            this.start_stop_BUT.Location = new System.Drawing.Point ( 16, 279 );
+            this.start_stop_BUT.Name = "start_stop_BUT";
+            this.start_stop_BUT.Size = new System.Drawing.Size ( 75, 25 );
+            this.start_stop_BUT.TabIndex = 0;
+            this.start_stop_BUT.Tag = "start_stop";
+            this.start_stop_BUT.Text = "Stop";
+            this.start_stop_BUT.UseVisualStyleBackColor = false;
+            this.start_stop_BUT.Click += new System.EventHandler ( this.BUT_Click );
+            // 
+            // pulse_BUT
+            // 
+            this.pulse_BUT.BackColor = System.Drawing.SystemColors.Control;
+            this.pulse_BUT.Location = new System.Drawing.Point ( 138, 279 );
+            this.pulse_BUT.Name = "pulse_BUT";
+            this.pulse_BUT.Size = new System.Drawing.Size ( 75, 25 );
+            this.pulse_BUT.TabIndex = 1;
+            this.pulse_BUT.Tag = "pulse";
+            this.pulse_BUT.Text = "Pulse";
+            this.pulse_BUT.UseVisualStyleBackColor = false;
+            this.pulse_BUT.Click += new System.EventHandler ( this.BUT_Click );
+            // 
+            // panel_background_BUT
+            // 
+            this.panel_background_BUT.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel_background_BUT.Location = new System.Drawing.Point ( 160, 21 );
+            this.panel_background_BUT.Name = "panel_background_BUT";
+            this.panel_background_BUT.Size = new System.Drawing.Size ( 25, 25 );
+            this.panel_background_BUT.TabIndex = 2;
+            this.panel_background_BUT.Tag = "panel_background_color";
+            this.panel_background_BUT.UseVisualStyleBackColor = false;
+            this.panel_background_BUT.Click += new System.EventHandler ( this.BUT_Click );
+            // 
+            // control_height_LAB
+            // 
+            this.control_height_LAB.AutoSize = true;
+            this.control_height_LAB.BackColor = System.Drawing.Color.Transparent;
+            this.control_height_LAB.Location = new System.Drawing.Point ( 55, 11 );
+            this.control_height_LAB.Name = "control_height_LAB";
+            this.control_height_LAB.Size = new System.Drawing.Size ( 98, 17 );
+            this.control_height_LAB.TabIndex = 4;
+            this.control_height_LAB.Text = "Control Height";
+            // 
+            // control_height_NUD
+            // 
+            this.control_height_NUD.Location = new System.Drawing.Point ( 160, 8 );
+            this.control_height_NUD.Maximum = new decimal ( new int [ ] {
+            200,
+            0,
+            0,
+            0} );
+            this.control_height_NUD.Minimum = new decimal ( new int [ ] {
+            50,
+            0,
+            0,
+            0} );
+            this.control_height_NUD.Name = "control_height_NUD";
+            this.control_height_NUD.Size = new System.Drawing.Size ( 53, 22 );
+            this.control_height_NUD.TabIndex = 5;
+            this.control_height_NUD.Tag = "control_height";
+            this.control_height_NUD.Value = new decimal ( new int [ ] {
+            200,
+            0,
+            0,
+            0} );
+            this.control_height_NUD.ValueChanged += new System.EventHandler ( this.NUD_ValueChanged );
+            // 
+            // panel_background_LAB
+            // 
+            this.panel_background_LAB.AutoSize = true;
+            this.panel_background_LAB.Location = new System.Drawing.Point ( 15, 25 );
+            this.panel_background_LAB.Name = "panel_background_LAB";
+            this.panel_background_LAB.Size = new System.Drawing.Size ( 124, 17 );
+            this.panel_background_LAB.TabIndex = 6;
+            this.panel_background_LAB.Text = "Panel Background";
+            // 
+            // colors_GB
+            // 
+            this.colors_GB.BackColor = System.Drawing.Color.Transparent;
+            this.colors_GB.Controls.Add ( this.indicator_color_BUT );
+            this.colors_GB.Controls.Add ( this.indicator_LAB );
+            this.colors_GB.Controls.Add ( this.panel_background_BUT );
+            this.colors_GB.Controls.Add ( this.panel_background_LAB );
+            this.colors_GB.Location = new System.Drawing.Point ( 13, 38 );
+            this.colors_GB.Name = "colors_GB";
+            this.colors_GB.Size = new System.Drawing.Size ( 200, 92 );
+            this.colors_GB.TabIndex = 7;
+            this.colors_GB.TabStop = false;
+            this.colors_GB.Text = "Colors";
+            // 
+            // indicator_color_BUT
+            // 
+            this.indicator_color_BUT.BackColor = System.Drawing.Color.White;
+            this.indicator_color_BUT.Location = new System.Drawing.Point ( 160, 55 );
+            this.indicator_color_BUT.Name = "indicator_color_BUT";
+            this.indicator_color_BUT.Size = new System.Drawing.Size ( 25, 25 );
+            this.indicator_color_BUT.TabIndex = 8;
+            this.indicator_color_BUT.Tag = "indicator_color";
+            this.indicator_color_BUT.UseVisualStyleBackColor = false;
+            this.indicator_color_BUT.Click += new System.EventHandler ( this.BUT_Click );
+            // 
+            // indicator_LAB
+            // 
+            this.indicator_LAB.AutoSize = true;
+            this.indicator_LAB.Location = new System.Drawing.Point ( 77, 59 );
+            this.indicator_LAB.Name = "indicator_LAB";
+            this.indicator_LAB.Size = new System.Drawing.Size ( 62, 17 );
+            this.indicator_LAB.TabIndex = 7;
+            this.indicator_LAB.Text = "Indicator";
+            // 
+            // indicator_type_GB
+            // 
+            this.indicator_type_GB.BackColor = System.Drawing.Color.Transparent;
+            this.indicator_type_GB.Controls.Add ( this.pulsed_RB );
+            this.indicator_type_GB.Controls.Add ( this.animated_RB );
+            this.indicator_type_GB.Location = new System.Drawing.Point ( 91, 138 );
+            this.indicator_type_GB.Name = "indicator_type_GB";
+            this.indicator_type_GB.Size = new System.Drawing.Size ( 122, 100 );
+            this.indicator_type_GB.TabIndex = 8;
+            this.indicator_type_GB.TabStop = false;
+            this.indicator_type_GB.Text = "Indicator Type";
+            // 
+            // pulsed_RB
+            // 
+            this.pulsed_RB.AutoSize = true;
+            this.pulsed_RB.Location = new System.Drawing.Point ( 30, 63 );
+            this.pulsed_RB.Name = "pulsed_RB";
+            this.pulsed_RB.Size = new System.Drawing.Size ( 69, 21 );
+            this.pulsed_RB.TabIndex = 1;
+            this.pulsed_RB.Tag = "pulsed";
+            this.pulsed_RB.Text = "Pulsed";
+            this.pulsed_RB.UseVisualStyleBackColor = true;
+            // 
+            // animated_RB
+            // 
+            this.animated_RB.AutoSize = true;
+            this.animated_RB.Checked = true;
+            this.animated_RB.Location = new System.Drawing.Point ( 30, 29 );
+            this.animated_RB.Name = "animated_RB";
+            this.animated_RB.Size = new System.Drawing.Size ( 85, 21 );
+            this.animated_RB.TabIndex = 0;
+            this.animated_RB.TabStop = true;
+            this.animated_RB.Tag = "animated";
+            this.animated_RB.Text = "Animated";
+            this.animated_RB.UseVisualStyleBackColor = true;
+            this.animated_RB.CheckedChanged += new System.EventHandler ( this.animated_RB_CheckedChanged );
+            // 
+            // refresh_interval_LAB
+            // 
+            this.refresh_interval_LAB.AutoSize = true;
+            this.refresh_interval_LAB.Location = new System.Drawing.Point ( 13, 248 );
+            this.refresh_interval_LAB.Name = "refresh_interval_LAB";
+            this.refresh_interval_LAB.Size = new System.Drawing.Size ( 140, 17 );
+            this.refresh_interval_LAB.TabIndex = 9;
+            this.refresh_interval_LAB.Text = "Refresh Interval (ms)";
+            // 
+            // refresh_interval_NUD
+            // 
+            this.refresh_interval_NUD.AllowDrop = true;
+            this.refresh_interval_NUD.Increment = new decimal ( new int [ ] {
+            5,
+            0,
+            0,
+            0} );
+            this.refresh_interval_NUD.Location = new System.Drawing.Point ( 160, 246 );
+            this.refresh_interval_NUD.Maximum = new decimal ( new int [ ] {
+            200,
+            0,
+            0,
+            0} );
+            this.refresh_interval_NUD.Minimum = new decimal ( new int [ ] {
+            10,
+            0,
+            0,
+            0} );
+            this.refresh_interval_NUD.Name = "refresh_interval_NUD";
+            this.refresh_interval_NUD.Size = new System.Drawing.Size ( 47, 22 );
+            this.refresh_interval_NUD.TabIndex = 10;
+            this.refresh_interval_NUD.Tag = "refresh_interval";
+            this.refresh_interval_NUD.Value = new decimal ( new int [ ] {
+            200,
+            0,
+            0,
+            0} );
+            this.refresh_interval_NUD.ValueChanged += new System.EventHandler ( this.NUD_ValueChanged );
+            // 
+            // control_PAN
+            // 
+            this.control_PAN.BackColor = System.Drawing.Color.RoyalBlue;
+            this.control_PAN.Controls.Add ( this.win8_progress_ring );
+            this.control_PAN.Location = new System.Drawing.Point ( 227, 49 );
+            this.control_PAN.Name = "control_PAN";
+            this.control_PAN.Size = new System.Drawing.Size ( 250, 250 );
+            this.control_PAN.TabIndex = 11;
+            // 
+            // win8_progress_ring
+            // 
+            this.win8_progress_ring.Location = new System.Drawing.Point ( 27, 25 );
+            this.win8_progress_ring.Name = "win8_progress_ring";
+            this.win8_progress_ring.Refresh_Rate = 100;
+            this.win8_progress_ring.Size = new System.Drawing.Size ( 100, 100 );
+            this.win8_progress_ring.TabIndex = 3;
+            // 
+            // exit_BUT
+            // 
+            this.exit_BUT.BackColor = System.Drawing.SystemColors.Control;
+            this.exit_BUT.Location = new System.Drawing.Point ( 401, 314 );
+            this.exit_BUT.Name = "exit_BUT";
+            this.exit_BUT.Size = new System.Drawing.Size ( 75, 25 );
+            this.exit_BUT.TabIndex = 12;
+            this.exit_BUT.Tag = "exit";
+            this.exit_BUT.Text = "Exit";
+            this.exit_BUT.UseVisualStyleBackColor = false;
+            this.exit_BUT.Click += new System.EventHandler ( this.BUT_Click );
+            // 
+            // TestWin8ProgressRing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF ( 8F, 16F );
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size ( 490, 349 );
+            this.Controls.Add ( this.exit_BUT );
+            this.Controls.Add ( this.control_PAN );
+            this.Controls.Add ( this.refresh_interval_NUD );
+            this.Controls.Add ( this.refresh_interval_LAB );
+            this.Controls.Add ( this.indicator_type_GB );
+            this.Controls.Add ( this.colors_GB );
+            this.Controls.Add ( this.control_height_NUD );
+            this.Controls.Add ( this.control_height_LAB );
+            this.Controls.Add ( this.pulse_BUT );
+            this.Controls.Add ( this.start_stop_BUT );
+            this.Name = "TestWin8ProgressRing";
+            this.Text = "TestWin8ProgressRing";
+            ( ( System.ComponentModel.ISupportInitialize ) ( this.control_height_NUD ) ).EndInit ( );
+            this.colors_GB.ResumeLayout ( false );
+            this.colors_GB.PerformLayout ( );
+            this.indicator_type_GB.ResumeLayout ( false );
+            this.indicator_type_GB.PerformLayout ( );
+            ( ( System.ComponentModel.ISupportInitialize ) ( this.refresh_interval_NUD ) ).EndInit ( );
+            this.control_PAN.ResumeLayout ( false );
+            this.ResumeLayout ( false );
+            this.PerformLayout ( );
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_stop_BUT;
+        private System.Windows.Forms.Button pulse_BUT;
+        private System.Windows.Forms.Button panel_background_BUT;
+        private Win8ProgressRing.Win8_ProgressRing win8_progress_ring;
+        private System.Windows.Forms.Label control_height_LAB;
+        private System.Windows.Forms.NumericUpDown control_height_NUD;
+        private System.Windows.Forms.Label panel_background_LAB;
+        private System.Windows.Forms.GroupBox colors_GB;
+        private System.Windows.Forms.Button indicator_color_BUT;
+        private System.Windows.Forms.Label indicator_LAB;
+        private System.Windows.Forms.GroupBox indicator_type_GB;
+        private System.Windows.Forms.RadioButton pulsed_RB;
+        private System.Windows.Forms.RadioButton animated_RB;
+        private System.Windows.Forms.Label refresh_interval_LAB;
+        private System.Windows.Forms.NumericUpDown refresh_interval_NUD;
+        private System.Windows.Forms.Panel control_PAN;
+        private System.Windows.Forms.Button exit_BUT;
+
+        }
+    }
+
---
+++ TestWin8ProgressRing.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+using W8PR = Win8ProgressRing.Win8_ProgressRing;
+
+namespace TestWin8ProgressRing
+    {
+    
+    // ************************************ class TestWin8ProgressRing
+    
+    public partial class TestWin8ProgressRing : Form
+        {
+
+        // ********************************************* ExtractIconEx
+        
+        [DllImport("Shell32")]
+        public static extern int ExtractIconEx (
+                string  sFile,
+                int     iIndex,
+            out IntPtr  piLargeVersion,
+            out IntPtr  piSmallVersion,
+                int     amountIcons );
+                
+        // ***************************************** GetExecutableIcon
+        
+        Icon GetExecutableIcon ( )
+            {
+            IntPtr large;
+            IntPtr small;
+            
+            ExtractIconEx (     Application.ExecutablePath, 
+                                0, 
+                            out large, 
+                            out small, 
+                                1 );
+            return ( Icon.FromHandle ( small ) );
+            }
+
+        // *********************************** center_control_in_panel
+        
+        void center_control_in_panel ( )
+            {
+            int  x;
+            int  y;
+            
+            x = ( control_PAN.Width - win8_progress_ring.Width ) / 2;
+            y = ( control_PAN.Height - win8_progress_ring.Height ) / 2;
+            
+            win8_progress_ring.Location = new Point ( x, y );
+            }
+            
+        // ************************************** TestWin8ProgressRing
+        
+        public TestWin8ProgressRing ( )
+            {
+            string [ ]  pieces;
+            
+            InitializeComponent ( );
+
+            pieces = win8_progress_ring.ProductVersion.Split ( 
+                            new char [ ] { '.' }, 
+                            StringSplitOptions.RemoveEmptyEntries );
+            this.Text = String.Format (
+                            "Test Win8 Progress Ring V{0}.{1}",
+                            pieces [ 0 ],
+                            pieces [ 1 ] );
+
+            this.Icon = GetExecutableIcon ( );
+            
+            control_height_NUD.Value = 
+                win8_progress_ring.Control_Height;
+            center_control_in_panel ( );
+            
+            refresh_interval_NUD.Value =
+                win8_progress_ring.Refresh_Rate;
+
+            animated_RB.Checked = true;
+            pulsed_RB.Checked = false;
+            
+            start_stop_BUT.Enabled = true;
+            pulse_BUT.Enabled = false;
+            
+            if ( start_stop_BUT.Text.Equals ( "Stop" ) )
+                {
+                win8_progress_ring.Animate = true;
+                }
+            else
+                {
+                win8_progress_ring.Animate = false;
+                }
+            }
+
+        // ******************************** animated_RB_CheckedChanged
+        
+        private void animated_RB_CheckedChanged ( object    sender, 
+                                                  EventArgs e )
+            {
+
+            if ( animated_RB.Checked )
+                {
+                pulse_BUT.Enabled = false;
+                
+                start_stop_BUT.Enabled = true;
+                
+                win8_progress_ring.Animate = true;
+                }
+            else
+                {
+                pulse_BUT.Enabled = true;
+
+                start_stop_BUT.Enabled = false;
+
+                win8_progress_ring.Animate = false;
+                }
+            }
+
+        // ************************************** start_stop_but_Click
+        
+        void start_stop_BUT_Click ( )
+            {
+
+            if ( animated_RB.Checked )
+                {
+                if ( start_stop_BUT.Text.Equals ( "Stop" ) )
+                    {
+                    win8_progress_ring.Animate = false;
+                    start_stop_BUT.Text = "Start";
+                    }
+                else
+                    {
+                    win8_progress_ring.Animate = true;
+                    start_stop_BUT.Text = "Stop";
+                    }
+                }
+            }
+
+        // ********************************************** color_choice
+
+        Color color_choice ( Color  current_color )
+            {
+            Color       color = current_color;
+            ColorDialog color_dialog;
+
+            try
+                {
+                color_dialog = new ColorDialog ( );
+                color_dialog.AllowFullOpen = true;
+                color_dialog.AnyColor = true;
+                color_dialog.Color = current_color;
+                color_dialog.FullOpen = false;
+                color_dialog.ShowHelp = true;
+                color_dialog.SolidColorOnly = false;
+                
+                if ( color_dialog.ShowDialog ( ) == DialogResult.OK )
+                    {
+                    color = color_dialog.Color;
+                    }
+                    
+                color_dialog.Dispose ( );
+                }
+            catch
+                {
+                color = current_color;
+                }
+            
+            return ( color ) ;
+            }
+            
+        // ************************************************* BUT_Click
+        
+        private void BUT_Click ( object    sender, 
+                                 EventArgs e )
+            {
+            Button  button = ( Button ) sender;
+            string  tag = button.Tag.ToString ( ).
+                                     ToLower ( ).
+                                     Trim ( );
+            
+            switch ( tag )
+                {
+                case "exit":
+                    Application.Exit ( );
+                    break;
+
+                case "indicator_color":
+                    {
+                    Color  chosen_color = color_choice ( 
+                                                button.BackColor );
+
+                    button.BackColor = chosen_color;
+                    win8_progress_ring.Indicator_Color = chosen_color;
+                    }
+                    break;
+                    
+                case "panel_background_color":
+                    {
+                    Color  chosen_color = color_choice ( 
+                                                button.BackColor );
+
+                    button.BackColor = chosen_color;
+                    control_PAN.BackColor = chosen_color;
+                    }
+                    break;
+                    
+                case "pulse":
+                    if ( pulsed_RB.Checked )
+                        {
+                        win8_progress_ring.Pulse ( );
+                        }
+                    break;
+
+                case "start_stop":
+                    start_stop_BUT_Click ( );
+                    break;
+
+                default:
+                
+                    break;
+                }
+            }
+
+        // ****************************************** NUD_ValueChanged
+        
+        void NUD_ValueChanged ( object    sender, 
+                                EventArgs e )
+            {
+            NumericUpDown nud = ( NumericUpDown ) sender;
+            string        tag = nud.Tag.ToString ( ).
+                                        ToLower ( ).
+                                        Trim ( );
+            
+            switch ( tag )
+                {
+                case "control_height":
+                    win8_progress_ring.Control_Height = 
+                        Convert.ToInt32 ( nud.Value );
+                    center_control_in_panel ( );
+                    break;
+                    
+                case "refresh_interval":
+                    win8_progress_ring.Refresh_Rate = 
+                        Convert.ToInt32 ( nud.Value );
+                    break;
+                    
+                default:
+                    
+                    break;
+                    
+                }
+            }
+            
+        } // class TestWin8ProgressRing
+        
+    } // namespace TestWin8ProgressRing
